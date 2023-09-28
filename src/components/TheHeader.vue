e04980a4d6f1d5acb8580723d27a0c0e8639c954
Pedro Henrique Lomba <p.henrique.lomba@gmail.com>
2023-09-28 18:00:51 -0300

    Primeiro Commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+    },
+    onBeforeMount(){
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    onBeforeUnmount(){
+        console.log('beforeUnmount');
+    },
+    unmounted(){
+        console.log('unmounted');
+    }
+}
+</script>
+
+<style>
+
+</style>
